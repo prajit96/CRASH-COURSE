d7032be19cab4666bbf8958bd4876ace9ef30ab8
prajit96 <santraprajit@gmail.com>
2024-04-17 13:54:40 +0530

    Creating and Using a Custom Hook for Data Fetching done

---
+++ DAY_10_MERN/Problem 2/post-list-component/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import PostsList from './PostsList';
+
+const App = () => {
+  return (
+    <div className="App">
+      <PostsList />
+    </div>
+  );
+};
+
+export default App;
---
+++ DAY_10_MERN/Problem 2/post-list-component/src/PostsList.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import useFetch from './useFetch';
+
+const PostsList = () => {
+  const { data, loading, error } = useFetch('https://jsonplaceholder.typicode.com/posts');
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error: {error.message}</p>;
+
+  return (
+    <div>
+      <h2>Posts List</h2>
+      <ul>
+        {data.map((post) => (
+          <li key={post.id}>{post.title}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default PostsList;
